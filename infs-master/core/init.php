88cf0e8dfe9828b7d478e25254e0294d916dc146
unknown <jkvapil6@gmail.com>
2020-04-07 00:06:17 +0200

    commit

--- infs-master/core/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-  include 'config/config.php';
-  include 'Database.php';
-
-  include 'helpers/helpers.php';
-  include 'helpers/formaters.php';
- ?>
--- infs-master/view/admin-edit-post.php
+++
@@ -1,7 +0,0 @@
-
-
-
-<form class="search-form" action="" method="post">
-  <input type="text" name="search" value="search.."></input>
-  <button type="submit" name="search-submit-button" >search</button>
-</form>
